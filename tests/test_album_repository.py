8a302f6f35fe38d5d0cddb7cf4552a74dca5c829
mahmudur mahid <m.mahid@icloud.com>
2026-02-16 11:13:10 +0000

    tailored all existing tests to correct data

--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -6,24 +6,16 @@ When we call AlbumRepository#all
 We get a list of Album objects reflecting the seed data
 """
 def test_get_all_album_records(db_connection):
-    db_connection.seed("seeds/music_library.sql")
+    db_connection.seed("seeds/records_store.sql")
     repository = AlbumRepository(db_connection)
 
     albums = repository.all()
 
     assert albums == [
-    Album(1, "Doolittle", 1989, 1),
-    Album(2, "Surfer Rosa", 1988, 1),
-    Album(3, "Waterloo", 1974, 2),
-    Album(4, "Super Trouper", 1980, 2),
-    Album(5, "Bossanova", 1990, 1),
-    Album(6, "Lover", 2019, 3),
-    Album(7, "Folklore", 2020, 3),
-    Album(8, "I Put a Spell on You", 1965, 4),
-    Album(9, "Baltimore", 1978, 4),
-    Album(10, "Here Comes the Sun", 1971, 4),
-    Album(11, "Fodder on My Wings", 1982, 4),
-    Album(12, "Ring Ring", 1973, 2)
+    Album(1, 'Neon Skies', 2020, 1),
+    Album(2, 'Broken Satellites', 2016, 2),
+    Album(3, 'Midnight Echoes', 2018, 3),
+    Album(4, 'Golden Static', 2022, 4)
 ]
 
 """
@@ -31,44 +23,36 @@ When we call AlbumRepository#find_album
 We get a single Album object reflecting the seed data.
 """
 def test_get_single_album(db_connection):
-    db_connection.seed("seeds/music_library.sql")
+    db_connection.seed("seeds/records_store.sql")
     repository = AlbumRepository(db_connection)
 
     album = repository.find_album(3)
-    assert album == Album(3, "Waterloo", 1974, 2)
+    assert album == Album(3, 'Midnight Echoes', 2018, 3)
 
 """
 When I call #find on the AlbumRepository with an id
 I am returned an artist with a corresponding id
 """
 def test_find_album(db_connection):
-    db_connection.seed("seeds/music_library.sql")
+    db_connection.seed("seeds/records_store.sql")
     repository = AlbumRepository(db_connection)
-    assert repository.find_album(4) == Album(4, "Super Trouper", 1980, 2)
+    assert repository.find_album(4) == Album(4, 'Golden Static', 2022, 4)
 
 """
 When we call AlbumRepository#create_album
 We get a new record in the database.
 """
 def test_create_single_album(db_connection):
-   db_connection.seed("seeds/music_library.sql")
+   db_connection.seed("seeds/records_store.sql")
    repository = AlbumRepository(db_connection)
 
-   repository.create_album(Album(None, "The Life of a Showgirl", 2025, 3))
+   repository.create_album(Album(None, "Voyage", 2022, 2))
    assert repository.all() == [
-    Album(1, "Doolittle", 1989, 1),
-    Album(2, "Surfer Rosa", 1988, 1),
-    Album(3, "Waterloo", 1974, 2),
-    Album(4, "Super Trouper", 1980, 2),
-    Album(5, "Bossanova", 1990, 1),
-    Album(6, "Lover", 2019, 3),
-    Album(7, "Folklore", 2020, 3),
-    Album(8, "I Put a Spell on You", 1965, 4),
-    Album(9, "Baltimore", 1978, 4),
-    Album(10, "Here Comes the Sun", 1971, 4),
-    Album(11, "Fodder on My Wings", 1982, 4),
-    Album(12, "Ring Ring", 1973, 2),
-    Album(13, "The Life of a Showgirl", 2025, 3)
+    Album(1, 'Neon Skies', 2020, 1),
+    Album(2, 'Broken Satellites', 2016, 2),
+    Album(3, 'Midnight Echoes', 2018, 3),
+    Album(4, 'Golden Static', 2022, 4),
+    Album(5, 'Voyage', 2022, 2)
    ]
 
 """
@@ -76,21 +60,13 @@ When we call AlbumRepository#delete_album
 We delete a record in the database.
 """
 def test_delete_album(db_connection):
-    db_connection.seed("seeds/music_library.sql")
+    db_connection.seed("seeds/records_store.sql")
     repository = AlbumRepository(db_connection)
 
-    repository.delete_album(13)
+    repository.delete_album(5)
     assert repository.all() == [
-    Album(1, "Doolittle", 1989, 1),
-    Album(2, "Surfer Rosa", 1988, 1),
-    Album(3, "Waterloo", 1974, 2),
-    Album(4, "Super Trouper", 1980, 2),
-    Album(5, "Bossanova", 1990, 1),
-    Album(6, "Lover", 2019, 3),
-    Album(7, "Folklore", 2020, 3),
-    Album(8, "I Put a Spell on You", 1965, 4),
-    Album(9, "Baltimore", 1978, 4),
-    Album(10, "Here Comes the Sun", 1971, 4),
-    Album(11, "Fodder on My Wings", 1982, 4),
-    Album(12, "Ring Ring", 1973, 2),
+    Album(1, 'Neon Skies', 2020, 1),
+    Album(2, 'Broken Satellites', 2016, 2),
+    Album(3, 'Midnight Echoes', 2018, 3),
+    Album(4, 'Golden Static', 2022, 4),
    ]
