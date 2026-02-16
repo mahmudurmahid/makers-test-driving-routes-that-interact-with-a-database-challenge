001a050c13daaa3706250b82b5a7e509edfbb496
mahmudur mahid <m.mahid@icloud.com>
2026-02-16 12:05:53 +0000

    added test for post album route when no data is inputted to make a new album

--- tests/test_app.py
+++ tests/test_app.py
@@ -39,3 +39,23 @@ def test_post_new_album(db_connection, web_client):
         "Album(4, Golden Static, 2022, 4)"
         "Album(5, Voyage, 2022, 2)"
     )
+
+"""
+When I call POST /albums without adding album info,
+I am returned an error message
+"""
+def test_post_albums_with_no_data(db_connection, web_client):
+    db_connection.seed("seeds/records_store.sql")
+    post_response = web_client.post("/albums")
+
+    assert post_response.status_code == 400
+    assert post_response.data.decode('utf-8') == "You need to submit a title, release_year, and artist_id"
+
+    get_response = web_client.get("/albums")
+    assert get_response.status_code == 200
+    assert get_response.data.decode('utf-8') == (
+        "Album(1, Neon Skies, 2020, 1)"
+        "Album(2, Broken Satellites, 2016, 2)"
+        "Album(3, Midnight Echoes, 2018, 3)"
+        "Album(4, Golden Static, 2022, 4)"
+    )
